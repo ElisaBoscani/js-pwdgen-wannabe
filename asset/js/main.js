fb4fecb10406d872f754db8f0e5fd6fa5a5ace74
Elisa Boscani <elisabosc90@gmail.com>
2023-08-22 15:28:41 +0200

    done exercise

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,12 @@
 //chiediamo all'utente il nome
-
+const userName = prompt("scrivi il tuo nome");
+console.log(userName);
 //chiediamo il cognome
-
+const userSurname = prompt("scrivi il tuo cognome");
 //chiediamo il colore preferito
-
+const userColor = prompt("Scrivi il tuo colore preferito");
 //stampa il risultato in console
-
+console.log(userName + userSurname + userColor + "23");
 //stampa il risultato in pagina
+const userAnswers = document.getElementById("answers");
+userAnswers.innerHTML = userName + userSurname + userColor + "23";
