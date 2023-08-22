027c5a239914a7b209455a52968c9f8a13fbe6d6
Elisa Boscani <elisabosc90@gmail.com>
2023-08-22 18:36:05 +0200

    bonus

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,10 +3,15 @@ const userName = prompt("scrivi il tuo nome");
 console.log(userName);
 //chiediamo il cognome
 const userSurname = prompt("scrivi il tuo cognome");
+console.log(userSurname);
 //chiediamo il colore preferito
 const userColor = prompt("Scrivi il tuo colore preferito");
+console.log(userColor);
+//chiediamo chiave
+const userKey = prompt("Scrivi un numero");
 //stampa il risultato in console
-console.log(userName + userSurname + userColor + "23");
+console.log(userName + userSurname + userColor + (Number(userKey) + 23));
 //stampa il risultato in pagina
 const userAnswers = document.getElementById("answers");
-userAnswers.innerHTML = userName + userSurname + userColor + "23";
+userAnswers.innerHTML =
+  userName + userSurname + userColor + (Number(userKey) + 23);
